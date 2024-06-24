e8b1a45ce268064c336953e4d9b781f78cd8fafa
btknight <brian@knight-networks.com>
2024-06-24 18:54:14 -0500

    pe188 right answer

---
+++ pe188.py
@@ -0,0 +1,28 @@
+from math import log2
+from sympy import totient
+
+
+def find_tet(base: int, tetronent: int, mod: int) -> int:
+    """Finds a tetration modulo some number."""
+    # https://stackoverflow.com/questions/30713648/how-to-compute-ab-mod-m
+    if base % mod == 0:
+        return 0
+    if tetronent == 1:
+        return base
+    minimum = int(log2(mod))
+    tot = int(totient(mod))
+    tet_minus_1 = find_tet(base, tetronent - 1, tot)
+    if tet_minus_1 < minimum:
+        tet_minus_1 = minimum
+    return pow(base, tot + tet_minus_1, mod)
+
+
+def main():
+    base = 1777
+    tetronent = 1855
+    n = find_tet(base, tetronent, 10**8)
+    print('Result:', n)
+
+
+if __name__ == '__main__':
+    main()
